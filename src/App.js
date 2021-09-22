84246b2c9efc92469e559394f7545b5e2b0d1308
pastorcoder <zuma4good@gmail.com>
2021-09-22 13:59:34 +0100

    contactUs

--- src/App.js
+++ src/App.js
@@ -1,39 +1,61 @@
-import React, { useState } from 'react';
-import { BrowserRouter, BrowserRouter as Router, Redirect, Switch, Route, Link} from "react-router-dom";
+import React, { useState } from "react";
+import {
+  BrowserRouter,
+  BrowserRouter as Router,
+  Redirect,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 import { Home } from "./Components/Home";
 import { About } from "./Components/About";
-import {  ContactUs } from "./Components/ContactUs";
+import { ContactUs } from "./Components/ContactUs";
 import { NotFound } from "./Components/NotFound";
-import './App.css';
+import "./App.css";
 
 export default function App() {
-
-  const [login,setLogin] = useState(false);
-
+  const [login, setLogin] = useState(false);
 
   return (
-   <BrowserRouter>
-    <div className="App">
-    <h1>React Router Demo</h1>
+    <BrowserRouter>
+      <div className="App">
+        <h1>React Router Demo</h1>
 
+        <div className="navbar">
+          <Link to="/">Home</Link>
+          <br />
+          <Link
+            to="/contact"
+            
+          >
+            Contact Us
+          </Link>
+          <br />
+          <Link to="/about">About</Link>
+          <br />
+          <br />
+          <button onClick={() => setLogin(!login)}>
+            {login ? "Click Here To Logout" : "Click Here To Login"}
+          </button>
+        </div>
 
-    <div className="navbar">
-      <Link to="/">Home</Link><br />
-      <Link to="/contact">Contact Us</Link><br />
-      <Link to="/about">About</Link><br /><br />
-      <button onClick={() => setLogin(!login)}>{login ? "Click Here To Logout" : "Click Here To Login"}</button>
-    </div>
-
-
-    <Switch>
-      <Route exact path="/"><Home /></Route>
-      {/* <Route path="/contact"><ContactUs login={login} /></Route> */}
-      <Route path="/contact">{login ? <ContactUs  /> : <Redirect to="/" />}</Route>
-      <Route path="/about"><About /></Route>
-      <Route path="*"><NotFound /></Route>
-    </Switch>
-    </div>
-   </BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          {/* <Route path="/contact"><ContactUs login={login} /></Route> */}
+          <Route path="/contact">
+            {login ? <ContactUs /> : <Redirect to="/" />}
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
